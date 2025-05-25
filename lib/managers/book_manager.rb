0321d16b7eef4182556388bd2878c7201398de97
Timur Kovalenko <timur.kovalenko@vinted.com>
2025-05-25 23:23:38 +0300

    Modify Book Manager class

--- lib/managers/book_manager.rb
+++ lib/managers/book_manager.rb
@@ -8,14 +8,16 @@ class BookManager
   end
 
   def load_books
+
     books_data = FileHandler.read_books_csv
     @books = []
-    books_data.each do |book_data|
+
+    books_data.each do |book|
       book = Book.new(
-        book_data[:id],
-        book_data[:title],
-        book_data[:author],
-        book_data[:release_year]
+        book[:id],
+        book[:title],
+        book[:author],
+        book[:release_year]
       )
       @books << book
     rescue StandardError => e
@@ -26,5 +28,36 @@ class BookManager
     puts "Error: #{e.message}"
     puts 'Check if books.csv is loaded'
     @books = []
+
   end
+
+  def display_all_books
+    if @books.empty?
+      puts 'No books available'
+      return
+    end
+    puts "\n" + '=' * 40
+    puts 'Available Books'
+    puts '=' * 40
+
+    @books.each_with_index do |book, i|
+      puts "#{i}. #{book}"
+    end
+
+    puts '=' * 40
+    puts "Books Amount: #{@books.count}"
+  end
+
+  def find_book_by_id(book_id)
+    @books.find { |book| book[:id] == book_id.to_i }
+  end
+
+  def get_all_books
+    @books.dup
+  end
+
+  def books_count
+    @books.count
+  end
+
 end
