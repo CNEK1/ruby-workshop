f7ce69f5205604237ffb37626c6c4986516ea048
Timur Kovalenko <timur.kovalenko@vinted.com>
2025-05-25 19:51:12 +0300

    Small test of book creation

--- main.rb
+++ main.rb
@@ -1,6 +1,8 @@
 require_relative 'lib/library_app'
-
+require_relative 'lib/models/book'
 begin
+  test = Book.new("1","2","3","4")
+  puts test
   app = LibraryApp.new
   app.run
 rescue StandardError => e
