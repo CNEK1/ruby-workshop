8f9a3a6dc56583bb446ffd501b23078804107632
Timur Kovalenko <timur.kovalenko@vinted.com>
2025-05-25 23:00:02 +0300

    Add Book Manager class

---
+++ lib/managers/book_manager.rb
@@ -0,0 +1,63 @@
+require_relative '../models/book'
+require_relative '../handlers/file_handler'
+
+class BookManager
+  def initialize
+    @books = []
+    load_books
+  end
+
+  def load_books
+
+    books_data = FileHandler.read_books_csv
+    @books = []
+
+    books_data.each do |book|
+      book = Book.new(
+        book[:id],
+        book[:name],
+        book[:author],
+        book[:release_year],
+      )
+      @books << book
+    rescue StandardError => e
+      puts "Error: #{e.message}"
+    end
+    puts "Loaded #{@books.count} books"
+  rescue StandardError => e
+    puts "Error: #{e.message}"
+    puts 'Check if books.csv is loaded'
+    @books = []
+
+  end
+
+  def display_all_books
+    if @books.empty?
+      puts 'No books available'
+      return
+    end
+    puts "\n" + '=' * 40
+    puts 'Available Books'
+    puts '=' * 40
+
+    @books.each_with_index do |book, i|
+      puts "#{i}. #{book}"
+    end
+
+    puts '=' * 40
+    puts "Books Amount: #{@books.count}"
+  end
+
+  def find_book_by_id(book_id)
+    @books.find { |book| book[:id] == book_id.to_i }
+  end
+
+  def get_all_books
+    @books.dup
+  end
+
+  def books_count
+    @books.count
+  end
+
+end
