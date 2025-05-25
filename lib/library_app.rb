d8a4fd7fd2cf69d27453141dc575f510a0c8b1cb
Timur Kovalenko <timur.kovalenko@vinted.com>
2025-05-25 18:16:53 +0300

    Creating Library App class

---
+++ lib/library_app.rb
@@ -0,0 +1,75 @@
+require_relative 'models/user'
+
+class LibraryApp
+  def initialize
+    @current_user = nil
+    puts 'Initialize of library app'
+  end
+
+  def run
+    display_welcome_message
+    authenticate_user
+    main_loop if @current_user
+  end
+
+  private
+
+  def display_welcome_message
+    puts "\n#{'=' * 30}"
+    puts 'Welcome to Library App'
+    puts '=' * 30
+  end
+
+  def authenticate_user
+    loop do
+      print 'Enter username: '
+      username = gets.chomp
+
+      begin
+        @current_user = User.new(username)
+        puts "Welcome #{username}!"
+        break
+      rescue ArgumentError => e
+        puts "Error: #{e.message}"
+        puts 'Try again'
+      end
+    end
+  end
+
+  def main_loop
+    loop do
+      display_menu
+      choice = get_user_choice
+      case choice
+      when 1
+        puts 'Function is not available'
+      when 2
+        puts 'Function is not available'
+      when 3
+        puts 'Function is not available'
+      when 4
+        puts "Bye, #{@current_user}!"
+        break
+      else
+        puts 'Invalid choice'
+      end
+    end
+  end
+
+  def display_menu
+    puts "\n#{'-' * 20}"
+    puts 'Main Menu'
+    puts '-' * 20
+    puts '1. List available books'
+    puts '2. Borrow a book'
+    puts '3. Return a book'
+    puts '4. Exit'
+    puts '-' * 20
+  end
+
+  def get_user_choice
+    puts 'Choose an action (1 - 4): '
+    input = gets.chomp
+    input.to_i
+  end
+end
