ad3241fce3d81b0870d20a3bd68c11bd7aa4ce85
Timur Kovalenko <timur.kovalenko@vinted.com>
2025-05-25 20:04:50 +0300

    Create a file handler, to handle csv files

---
+++ lib/handlers/file_handler.rb
@@ -0,0 +1,24 @@
+class FileHandler
+  def self.read_books_csv(filename = 'data/books.csv')
+    unless File.exist? filename
+      raise "File not found: #{filename}"
+    end
+
+    books_data = []
+
+    begin
+      CSV.foreach(filename, headers: true) do |row|
+        books_data << {
+          id: row['Book ID'],
+          title: row['Book Name'],
+          author: row['Author'],
+          release_year: row['Release Year']
+        }
+      end
+    rescue => e
+      raise "Error reading file #{filename}: #{e}"
+    end
+    books_data
+  end
+
+end
