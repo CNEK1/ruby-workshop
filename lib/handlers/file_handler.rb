b94191ec507970888ff1aada62ef96f662efc416
Timur Kovalenko <timur.kovalenko@vinted.com>
2025-05-26 13:26:02 +0300

    Adding read and write function to .db files

--- lib/handlers/file_handler.rb
+++ lib/handlers/file_handler.rb
@@ -4,6 +4,7 @@ class FileHandler
     unless File.exist? filename
       raise "File not found: #{filename}"
     end
+
     books_data = []
 
     begin
@@ -19,6 +20,27 @@ class FileHandler
       raise "Error reading file #{filename}: #{e}"
     end
     books_data
+  end
+
+  def self.read_from_db_file(filename)
+    return [] unless File.exist? filename
+
+    begin
+      File.readlines(filename).map(&:chomp).reject(&:empty?)
+    rescue => e
+      puts "Error reading file #{filename}: #{e}"
+      []
+    end
+  end
+
+  def self.write_to_db_file(filename, data)
+    begin
+      File.open(filename, 'w') do |f|
+        data.each {|line| f.puts(line)}
+      end
+    rescue => e
+      puts "Error writing file #{filename}: #{e}"
+    end
   end
 
 end
